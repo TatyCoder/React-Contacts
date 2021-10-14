ca7cecb9864a06f761f8e4184fccde737b8d945b
TatyCoder <tmilanes@gmail.com>
2021-10-14 14:43:10 -0400

    Adding a delete button

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import AddContact from './Components/Contacts/AddContact';
 import ContactsList from './Components/Contacts/ContactsList';
 
-const contacts = [{
-  id: 1,
-  name: "Name1",
-  address: {
-      street: "Street1",
-      city: "City1",
-      state: "State1",
-      zip: "Zip1"
+const contacts = [
+  {
+    id: 1,
+    name: 'Name1',
+    address: {
+      street: 'Street1',
+      city: 'City1',
+      state: 'State1',
+      zip: 'Zip1',
+    },
+    phone: 'Phone1',
   },
-  phone: "Phone1"
-},
-{
-  id: 2,
-  name: "Name2",
-  address: {
-      street: "Street2",
-      city: "City2",
-      state: "State2",
-      zip: "Zip2",
+  {
+    id: 2,
+    name: 'Name2',
+    address: {
+      street: 'Street2',
+      city: 'City2',
+      state: 'State2',
+      zip: 'Zip2',
+    },
+    phone: 'Phone2',
   },
-  phone: "Phone2"
-},
-{
-  
-  id: 3,
-  name: "Name3",
-  address: {
-      street: "Street3",
-      city: "City3",
-      state: "State3",
-      zip: "Zip3",
+  {
+    id: 3,
+    name: 'Name3',
+    address: {
+      street: 'Street3',
+      city: 'City3',
+      state: 'State3',
+      zip: 'Zip3',
+    },
+    phone: 'Phone3',
   },
-  phone: "Phone3"
-}
-]
+];
 
 function App() {
+  const [contactsList, setContactsList] = useState(contacts);
+
+  const deleteContactHandler = (event) => {
+    const newList = contacts.filter((c) => c.id.toString() !== event.target.id);
+    // Optional way:
+    // const newList = [];
+    // for (let c = 0; c < contacts.length; c++) {
+    //   if (contacts[c].id.toString() !== event.target.id) {
+    //     newList.push(contacts[c]);
+    //   }
+    // }
+    console.log(newList);
+    setContactsList(newList);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -47,7 +63,7 @@ function App() {
       </header>
       <h2 className="App-h2">Contacts</h2>
       <AddContact />
-      <ContactsList contacts={contacts} />
+      <ContactsList contacts={contactsList} onDelete={deleteContactHandler} />
     </div>
   );
 }
--- src/Components/Contacts/ContactsList.js
+++ src/Components/Contacts/ContactsList.js
@@ -1,5 +1,5 @@
 import Card from '../UI/Card';
-import styles from './ContactsList.module.css'
+import styles from './ContactsList.module.css';
 
 const ContactsList = (props) => {
   console.log(props.contacts);
@@ -7,7 +7,16 @@ const ContactsList = (props) => {
     <Card className={styles.contacts}>
       <ul>
         {props.contacts.map((contacts) => (
-          <li key={contacts.id}>{contacts.name}</li>
+          <li key={contacts.id}>
+            {contacts.name}
+            <button
+              id={contacts.id}
+              className={styles.button}
+              onClick={props.onDelete}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </Card>
